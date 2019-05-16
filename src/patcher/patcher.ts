41f25f83ec08fcbbf378265ea13c220e699eed44
Patrick Muff <muff.patrick@muffag.ch>
2019-05-16 14:18:30 +0200

    docs(patcher): comment functions

--- src/patcher/patcher.ts
+++ src/patcher/patcher.ts
@@ -2,6 +2,9 @@ import { IPatch } from '../scanner/scanner.interface';
 import { readFile, writeFile } from 'fs-extra';
 import { join } from 'path';
 
+/**
+ * Used to apply a given IPatch to a target directory.
+ */
 export async function applyPatch(patch: IPatch, targetDirectory: string) {
   await mergePackageJson(
     join(targetDirectory, 'package.json'),
@@ -9,16 +12,23 @@ export async function applyPatch(patch: IPatch, targetDirectory: string) {
   );
 }
 
+/**
+ * Patches a package.json file at `sourcePath` with the file at `targetPath`.
+ * @param sourcePath Original package.json path
+ * @param targetPath Patch package.json
+ */
 async function mergePackageJson(sourcePath: string, targetPath: string) {
+  // Read and parse both JSON files into objects
   const [sourcePackageJson, targetPackageJson] = await Promise.all([
     readFile(sourcePath),
     readFile(targetPath)
   ]);
-
   const parsedSource = JSON.parse(sourcePackageJson.toString());
   const parsedTarget = JSON.parse(targetPackageJson.toString());
 
+  // Assigned the target to the source
   const merged = Object.assign(parsedSource, parsedTarget);
 
+  // Stringify merged object and write to source path
   await writeFile(sourcePath, JSON.stringify(merged, null, 2));
 }
--- src/scanner/scanner.interface.ts
+++ src/scanner/scanner.interface.ts
@@ -1,5 +1,8 @@
 export interface IPatch {
+  // Provided values in manifest.json
   name: string;
   description: string;
+
+  // Generated properties
   directory: string;
 }
