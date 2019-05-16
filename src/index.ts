09fec7d8cdc9f8d74963bc9f52f5078bd24bf952
Patrick Muff <muff.patrick@muffag.ch>
2019-05-16 10:20:47 +0200

    feat(questions): format questions
    Make questions look nicer by choosing a `checkbox` type for the inquirer
    question. Also refactor them into one file with better typings.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,48 @@
+import 'colors';
 import { prompt } from 'inquirer';
 import { getAllMetas } from './patch';
-import { getQuestions } from './questions';
+import { QuestionType } from './question-type';
+import { exec } from 'child_process';
 
 const start = async () => {
-  const metas = await getAllMetas();
-  const answers = await prompt(getQuestions(metas));
+  const patches = await getAllMetas();
 
-  console.log(answers);
+  const answers: {
+    [QuestionType.PATH]: string;
+    [QuestionType.PATCHTYPE]: string[];
+  } = await prompt([
+    {
+      name: QuestionType.PATH,
+      message: 'Enter target directory',
+      default: process.cwd()
+    },
+    <any>{
+      name: QuestionType.PATCHTYPE,
+      type: 'checkbox',
+      message: 'Select patches to apply',
+      choices: patches.map(patch => ({
+        name: patch.name + ' ' + patch.description.grey,
+        checked: false
+      })),
+      filter: (values: string[]) => {
+        // Remove description from answer
+        return values.map(value => value.replace(/\s.+$/, ''));
+      }
+    }
+  ]);
+
+  const chosenPatches = answers[QuestionType.PATCHTYPE];
+
+  console.log(
+    `📦 Applying ${chosenPatches.length > 1 ? 'patches' : 'patch'}: ` +
+      chosenPatches.map(answer => answer.cyan).join(', ')
+  );
+
+  console.log('📦 Running command: ' + 'npm install -s'.bgYellow.black);
+
+  const child = exec('npm install -s');
+  child.stdout!.pipe(process.stdout);
+  child.stderr!.pipe(process.stderr);
 };
 
 start();
